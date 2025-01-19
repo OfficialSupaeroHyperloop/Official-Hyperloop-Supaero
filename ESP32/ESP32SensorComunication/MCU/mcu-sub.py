d4e3fc4cd47f996b1243c877a188413ce507200f
sfarrelardan <farrelsusilo@pm.me>
2025-01-19 22:21:06 +0100

    Added sub to MQTT script for MCU

---
+++ ESP32/ESP32SensorComunication/MCU/mcu-sub.py
@@ -0,0 +1,20 @@
+import paho.mqtt.client as paho
+
+def on_message(mosq, obj, msg):
+    print("topic: ", msg.topic, "qos: ", msg.qos, "payload: ", msg.payload, "\n")
+    mosq.publish('sensor/dist_ack', 'ack', 0)
+
+def on_publish(mosq, obj, mid):
+    pass
+
+if __name__ == '__main__':
+    client = paho.Client()
+    client.on_message = on_message
+    client.on_publish = on_publish
+
+    client.connect("10.212.93.39", 1883, 60)
+
+    client.subscribe("sensor/distance", 1)
+
+    while client.loop() == 0:
+        pass
